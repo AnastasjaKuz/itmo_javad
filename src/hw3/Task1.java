7da29bb024b07df840c5d9c7c8befdc8d00e675e
Anastasja <AnastasjaKuz@yandex.ru>
2022-09-28 10:33:08 +0300

    сделала дз3

--- src/hw3/Task1.java
+++ src/hw3/Task1.java
@@ -16,7 +16,7 @@ public class Task1 {
         int tarelka1 = tarelka.nextInt();
         double sredstvo1 = sredstvo.nextDouble();
 
-        while (tarelka1 > 0 && sredstvo1 > 0){
+        while (tarelka1 > 0 && sredstvo1 > 0) {
             tarelka1--;
             sredstvo1 -= 0.5;
             System.out.println(tarelka1);
@@ -24,7 +24,30 @@ public class Task1 {
         }
         System.out.println(tarelka1);
         System.out.println(sredstvo1);
-    }
+
+
+        int unknownumber, usernumber, truecount = 0;
+        System.out.println("Угадай число");
+        unknownumber = (int) Math.floor(Math.random() * 9);
+        do {
+            truecount++;
+            Scanner input = new Scanner(System.in);
+            usernumber = input.nextInt();
+            if (unknownumber > usernumber) {
+                System.out.println("загаданное число больше");
+            } else if (unknownumber < usernumber) {
+                System.out.println("загаданное число меньше");
+            } else if (usernumber == unknownumber) {
+                System.out.println("Вы угадали");
+            }
+        } while (unknownumber != usernumber);
+        System.out.println("количество попыток" + truecount);
+
+
 
     }
+}
+
+
+
 
